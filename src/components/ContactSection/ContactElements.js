796be0eb438c590b86f0d949883dc700135061fa
AshMagill <ashmagillnz@gmail.com>
2022-02-13 14:54:40 +1300

    tidied up form css

--- src/components/ContactSection/ContactElements.js
+++ src/components/ContactSection/ContactElements.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
-  min-height: 692px;
   position: fixed;
   bottom: 0;
   left: 0;
@@ -18,23 +17,16 @@ export const FormWrap = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-
-  @media screen and (max-width: 400px) {
-    height: 80%;
-  }
 `;
 
 export const Icon = styled(Link)`
   margin-left: 32px;
-  margin-top: 32px;
+  margin-top: 1rem;
   text-decoration: none;
   color: #32302f;
-  font-weight: 700;
-  font-size: 32px;
+  font-size: 28px;
 
   @media screen and (max-width: 480px) {
-    margin-left: 16px;
-    margin-top: 8px;
   }
 `;
 
@@ -52,63 +44,63 @@ export const FormContent = styled.div`
 export const Form = styled.form`
   background: #32302f;
   max-width: 400px;
-  height: auto;
+  height: 90%;
   width: 100%;
   z-index: 1;
   display: grid;
   margin: 0 auto;
-  padding: 80px 32px;
-  border-radius: 4px;
+  padding: 2rem;
+  border-radius: 0.5rem;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
 
   @media screen and (max-width: 400px) {
-    padding: 32px 32px;
   }
 `;
 
 export const FormH1 = styled.h1`
-  margin-bottom: 40px;
   color: #fbf1c7;
-  font-size: 20px;
+  font-size: 28px;
   font-weight: 400;
   text-align: center;
+  margin-bottom: 1rem;
+  @media screen and (max-width: 480px) {
+    font-size: 28px;
+  }
 `;
 
 export const FormLabel = styled.label`
-  margin-bottom: 8px;
-  font-size: 14px;
+  font-size: 18px;
   color: #fbf1c7;
+  height: 2rem;
 `;
 export const FormInput = styled.input`
-  padding: 16px 16px;
-  margin-bottom: 32px;
+  margin-bottom: 1rem;
   border: none;
-  border-radius: 4px;
+  border-radius: 0.5rem;
   background: #fbf1c7;
+  height: 3rem;
 `;
 
 export const MessageInput = styled.textarea`
-  padding: 32px 16px;
-  margin-bottom: 32px;
+  margin-bottom: 1rem;
   border: none;
-  border-radius: 4px;
+  border-radius: 0.5rem;
   background: #fbf1c7;
+  height: 15rem;
   width: 100%;
-  resize: vertical;
 `;
 
 export const FormButton = styled.button`
   background: #83a598;
-  padding: 16px 0;
   border: none;
   border-radius: 0.5rem;
   color: #32302f;
-  font-size: 20px;
+  font-size: 18px;
   cursor: pointer;
+  height: 3rem;
+  margin-top: 1rem;
 `;
 export const Text = styled.span`
   text-align: center;
-  margin-top: 24px;
   color: #fff;
-  font-size: 14px;
 `;
--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Container,
   FormWrap,
@@ -10,23 +10,22 @@ import {
   FormInput,
   FormButton,
   MessageInput,
-  Text,
-} from "./ContactElements";
+} from './ContactElements';
 
 const SignIn = () => {
   return (
     <>
       <Container>
         <FormWrap>
-          <Icon to="/">proshops.co.nz</Icon>
+          <Icon to='/'>proshops.co.nz</Icon>
           <FormContent>
-            <Form action="#">
+            <Form action='#'>
               <FormH1>Get in touch</FormH1>
-              <FormLabel htmlFor="for">Email</FormLabel>
-              <FormInput type="email" required />
-              <FormLabel htmlFor="for">Message</FormLabel>
-              <MessageInput type="message" required />
-              <FormButton type="submit">Send Message</FormButton>
+              <FormLabel>Email</FormLabel>
+              <FormInput type='email' required />
+              <FormLabel>Message</FormLabel>
+              <MessageInput type='message' required />
+              <FormButton type='submit'>Send Message</FormButton>
             </Form>
           </FormContent>
         </FormWrap>
@@ -36,53 +35,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
-//import React, { useState } from "react";
-//import Button from "../ButtonElement";
-//import Navbar from "../Navbar";
-//import Sidebar from "../Sidebar";
-
-//import {
-//Container,
-//FormWrap,
-//FormContent,
-//Form,
-//FormH1,
-//FormLabel,
-//FormInput,
-//FormButton,
-//MessageInput,
-//} from "./ContactElements";
-
-//const Contact = () => {
-//const [isOpen, SetIsOpen] = useState(false);
-
-//const toggle = () => {
-//SetIsOpen(!isOpen);
-//};
-//return (
-//<>
-//<Sidebar isOpen={isOpen} toggle={toggle} />
-//<Navbar toggle={toggle} />
-//<Container>
-//<FormWrap>
-//<FormContent>
-//<Form action="#">
-//<FormH1>Get In Touch</FormH1>
-//<FormLabel htmlFor="for">Name</FormLabel>
-//<FormInput type="email" required />
-//<FormLabel htmlFor="for">Email</FormLabel>
-//<FormInput type="password" required />r{" "}
-//<FormLabel htmlFor="for">Message</FormLabel>
-//<MessageInput type="message" required />
-//<FormButton type="submit">Send</FormButton>
-//<FormButton type="reset">Reset</FormButton>
-//</Form>
-//</FormContent>
-//</FormWrap>
-//</Container>
-//</>
-//);
-//};
-
-//export default Contact;
