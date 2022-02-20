1411dd128225e84c2d5bc1dfc4778205bd725192
AshMagill <ashmagillnz@gmail.com>
2022-02-20 16:41:02 +1300

    backend of email form sorted

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { send } from 'emailjs-com';
 import {
   Container,
   FormWrap,
@@ -12,7 +13,33 @@ import {
   MessageInput,
 } from './ContactElements';
 
+const SERVICE_ID = 'service_pkfoe6o';
+const TEMPLATE_ID = 'template_dw6qj0n';
+const USER_ID = 'user_sV424rDcNiK8zdAEz0n3C';
+
 const SignIn = () => {
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(SERVICE_ID, TEMPLATE_ID, toSend, USER_ID)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
     <>
       <Container>
@@ -22,10 +49,24 @@ const SignIn = () => {
             <Form action='#'>
               <FormH1>Get in touch</FormH1>
               <FormLabel>Email</FormLabel>
-              <FormInput type='email' required />
+              <FormInput
+                type='email'
+                name='user_email'
+                placeholder='Email…'
+                required
+                onChange={handleChange}
+              />
               <FormLabel>Message</FormLabel>
-              <MessageInput type='message' required />
-              <FormButton type='submit'>Send Message</FormButton>
+              <MessageInput
+                type='text'
+                name='user_message'
+                placeholder='Message…'
+                required
+                onChange={handleChange}
+              />
+              <FormButton type='submit' onClick={onSubmit}>
+                Message
+              </FormButton>
             </Form>
           </FormContent>
         </FormWrap>
