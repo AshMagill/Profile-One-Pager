55c50557f230ffcd161a60776bf3fe5c9bb74e7d
AshMagill <ashmagillnz@gmail.com>
2022-02-20 20:51:48 +1300

    gallery

--- src/components/GallerySection/index.js
+++ src/components/GallerySection/index.js
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import HoverVideoPlayer from 'react-hover-video-player';
 
 import Cart from '../../videos/cart.mkv';
-import Ratings from '../../videos/ratings.mkv';
-import Carousel from '../../videos/carousel.mkv';
-import Pagination from '../../videos/pagination.mkv';
-import Search from '../../videos/search.mkv';
-import Orders from '../../videos/orders.mkv';
-import Product from '../../videos/product.mkv';
-import Users from '../../videos/users.mkv';
-import AOrder from '../../videos/orders_admin.mkv';
-import Delivered from '../../videos/delivered.mkv';
-import Checkout from '../../videos/checkout.mkv';
+//import Ratings from '../../videos/ratings.mkv';
+//import Carousel from '../../videos/carousel.mkv';
+//import Pagination from '../../videos/pagination.mkv';
+//import Search from '../../videos/search.mkv';
+//import Orders from '../../videos/orders.mkv';
+//import Product from '../../videos/product.mkv';
+//import Users from '../../videos/users.mkv';
+//import AOrder from '../../videos/orders_admin.mkv';
+//import Delivered from '../../videos/delivered.mkv';
+//import Checkout from '../../videos/checkout.mkv';
 
 import {
   GalleryGrid,
@@ -24,7 +24,6 @@ import {
 } from './GalleryElements';
 
 const GallerySection = () => {
-  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
   return (
     <GalleryContainer id='gallery'>
       <GalleryH1>Gallery</GalleryH1>
@@ -33,163 +32,18 @@ const GallerySection = () => {
         all fully customisable!
       </GalleryP>
       <GalleryGrid>
-        <button onClick={() => setIsVideoPlaying(!isVideoPlaying)}>
-          <HoverVideoPlayer
-            videoSrc={Cart}
-            focused={isVideoPlaying}
-            muted
-            none
-            pausedOverlay={
-              <GalleryVideo>
-                <GalleryText>
-                  <GalleryP2>Full featured shopping cart</GalleryP2>
-                </GalleryText>
-              </GalleryVideo>
-            }
-            overlayTransitionDuration={1000}
-          />
-        </button>
         <HoverVideoPlayer
-          videoSrc={Ratings}
+          videoSrc={Cart}
           muted
           none
           pausedOverlay={
             <GalleryVideo>
               <GalleryText>
-                <GalleryP2>Product Reviews and Ratings</GalleryP2>
+                <GalleryP2>Full featured shopping cart</GalleryP2>
               </GalleryText>
             </GalleryVideo>
           }
           overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={Carousel}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>Top products carousel</GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={Pagination}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>Product pagination</GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={Search}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>Product search feature</GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={Orders}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>User profile with orders</GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={Product}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>Admin Product managment</GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={Users}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>Admin user managment</GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={AOrder}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>Admin Order details page</GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={Delivered}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>Mark orders as delivered option</GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
-        />
-        <HoverVideoPlayer
-          videoSrc={Checkout}
-          muted
-          none
-          pausedOverlay={
-            <GalleryVideo>
-              <GalleryText>
-                <GalleryP2>
-                  Checkout process (shipping, paymemt method etc.)
-                </GalleryP2>
-              </GalleryText>
-            </GalleryVideo>
-          }
-          overlayTransitionDuration={1000}
-          videoStyle={{ borderRadius: '0.5rem' }}
         />
       </GalleryGrid>
     </GalleryContainer>
@@ -197,3 +51,145 @@ const GallerySection = () => {
 };
 
 export default GallerySection;
+//<HoverVideoPlayer
+//videoSrc={Ratings}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>Product Reviews and Ratings</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={Carousel}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>Top products carousel</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={Pagination}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>Product pagination</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={Search}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>Product search feature</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={Orders}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>User profile with orders</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={Product}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>Admin Product managment</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={Users}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>Admin user managment</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={AOrder}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>Admin Order details page</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={Delivered}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>Mark orders as delivered option</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
+//<HoverVideoPlayer
+//videoSrc={Checkout}
+//muted
+//none
+//pausedOverlay={
+//<GalleryVideo>
+//<GalleryText>
+//<GalleryP2>
+//Checkout process (shipping, paymemt method etc.)
+//</GalleryP2>
+//</GalleryText>
+//</GalleryVideo>
+//}
+//overlayTransitionDuration={1000}
+//videoStyle={{ borderRadius: '0.5rem' }}
+///>
